1676af0f089d514c2261827a466e88a2d277d854
Diptorr <78503363+Diptorr@users.noreply.github.com>
2023-04-11 23:04:16 +0200

    Update main.rs

--- Generait_cyka/src/main.rs
+++ Generait_cyka/src/main.rs
@@ -2,32 +2,45 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Write 1 to generait for first half");
+    println!("Write 0 to generait for first half");
     println!("Write 2 to generait for second half");
-    println!("Write 12 to generait for both");
+    println!("Write 11 to generait for both");
 
-    let pr_grupe: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    let dr_grupe = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    let halva:Vec<i32> = vec![1, 2];
+    let pr_skupina:[&str; 13]  = ["Kopejs", "Dan", "Hladik"," Stastny"," Kozma"," Ilija"," Karel"," Sauer"," Patrik"," Borovicka"," Julian"," Lucie"," Kamen"];
+    let dr_skupina:[&str; 14] = ["Terka"," Jasmin"," Davca"," Zikmund"," Hana"," Lorenc"," Lenka"," Matous"," Ondra"," Pavel"," Rene"," Terka"," Vera"," Tybl"];
+    let halva:[&str; 2] = ["Hlava", "Podradny"];
     let mut intp = String::new();
+    let mut intp_2 =String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut intp).expect("Failed to read line");
+    println!("Chtel by jsi i hlavni a vedlejsi roly?
+                        Jestli ano      zamackni 1
+                        Jestli ne       zamackni 0");
 
-    let intp: u32 = match guess.trim().parse() {
+    io::stdin().read_line(&mut intp_2).expect("Failed to read line");
+
+    let intp: u8 = match intp.trim().parse() {
         Ok(num) => num,
         Err(_) => 0,
     };
+    let intp_2: u8 = match intp_2.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+    let intp_fin = intp + intp_2;
 
     let halvni = rand::thread_rng().gen_range(0..=1);
     let sluzba_1 = rand::thread_rng().gen_range(0..13);
     let sluzba_2 = rand::thread_rng().gen_range(0..14);
 
-
     println!("Hlava otrocke operace 1.C je Quatarsky sheik Adam Tybl");
-    match guess {
-        1 => println!("{}", pr_grupe[sluzba_1]),
-        2 => println!("{}", dr_grupe[sluzba_2]),
-        12 => println!("{}, {}, {}", pr_grupe[sluzba_1], halva[halvni], dr_skupina[sluzba_2]),
+    match intp_fin {
+        0 => println!("{}", pr_skupina[sluzba_1]),
+        1 => println!("{} jako {}", pr_skupina[sluzba_1], halva[halvni]),
+        3 => println!("{} ", dr_skupina[sluzba_2]),
+        4 => println!("{} jako {}", pr_skupina[sluzba_2], halva[halvni]),
+        12 => println!("{} jakozto {}
+a  {}", pr_skupina[sluzba_1], halva[halvni],  dr_skupina[sluzba_2]),
         _ => println!("Ty jsi oplili obcane"),
     }
 }
